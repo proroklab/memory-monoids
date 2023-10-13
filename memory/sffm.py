51d0f179e13ceb85eb90eae961989af715d411fe
Steven Morad <sm2558@cam.ac.uk>
2023-10-13 12:01:19 +0100

    simplify, now stop tuning cartpole

--- memory/sffm.py
+++ memory/sffm.py
@@ -5,7 +5,6 @@ import equinox as eqx
 from equinox import nn
 
 import memory.ffa as ffa
-from modules import complex_symlog, linsymlog, mish, softsymlog, symlog
 
 
 class SFFM(eqx.Module):
@@ -19,8 +18,6 @@ class SFFM(eqx.Module):
     pre: nn.Linear
     skip: nn.Linear
     mix: nn.Linear
-    mag: nn.Linear
-    phase: nn.Linear
     ln: nn.LayerNorm
 
     def __init__(
@@ -36,20 +33,16 @@ class SFFM(eqx.Module):
         self.trace_size = trace_size
         self.context_size = context_size
 
-        _, k1, k2, k3, k4 = jax.random.split(key, 5)
+        _, k1, k2, k3, = jax.random.split(key, 4)
         self.pre = eqx.filter_vmap(nn.Linear(input_size, trace_size, key=k1))
         self.skip = eqx.filter_vmap(nn.Linear(input_size, self.output_size, key=k2))
         self.ffa_params = ffa.init(trace_size, context_size)
-        self.mix = eqx.filter_vmap(nn.Linear(trace_size * context_size, self.output_size, key=k3))
-        self.mag = eqx.filter_vmap(nn.Linear(trace_size * context_size, self.output_size, key=k3))
-        self.phase = eqx.filter_vmap(nn.Linear(trace_size * context_size, self.output_size, key=k4))
-        self.ln = eqx.filter_vmap(nn.LayerNorm(None, use_weight=False, use_bias=False))
+        self.mix = eqx.filter_vmap(nn.Linear(3 * trace_size * context_size, self.output_size, key=k3))
 
     @eqx.filter_jit
     def initial_state(self, shape=tuple()):
         return jnp.zeros((*shape, 1, self.trace_size, self.context_size), dtype=jnp.complex64)
 
-    # TODO: WTF why does adding another jit change performance...
     @eqx.filter_jit
     def __call__(
         self, x: jax.Array, state: jax.Array, start: jax.Array, next_done, key
@@ -58,12 +51,14 @@ class SFFM(eqx.Module):
         pre = pre / (1e-6 + jnp.linalg.norm(pre, axis=-1, keepdims=True, ord=2))
         state = ffa.apply(params=self.ffa_params, x=pre, state=state, start=start, next_done=next_done)
         s = state.reshape(state.shape[0], -1)
-        mag = mish(self.mag(jnp.log(1 + jnp.abs(s))))
-        phase = mish(self.phase(jnp.angle(s)))
-        z = self.mix(mag * phase) + self.skip(x)
+        magnitude = jnp.log(1 + jnp.abs(s))
+        angle = jnp.angle(s)
+        z = self.mix(
+            jnp.concatenate([magnitude, jnp.sin(angle), jnp.cos(angle)], axis=-1)
+        )
         
         final_state = state[-1:]
-        return z, final_state
+        return z + self.skip(x), final_state
 
 
 if __name__ == "__main__":
